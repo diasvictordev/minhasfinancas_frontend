f874a7a0f84adf7823f4fc9a877befc379d4327d
diasvictordev <victor.dias@aluno.ueg.br>
2023-07-11 21:23:32 -0300

    commit inicial front

---
+++ src/views/login.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Card from "../components/card";
+import FormGroup from "../components/form-group";
+
+class Login extends React.Component{
+
+    state = {
+        email: "",
+        senha: ""
+    }
+    entrar = () =>{
+            console.log(this.state.email)
+            console.log(this.state.senha)
+    }
+
+    cadastrar = () => {
+
+    }
+    render(){
+        return(
+        <div className="row">
+            <div className="col-md-6" style={{ position: 'relative', left: '300px' }}>
+                <div class="bs-docs-section">
+                    <Card title="Login">
+                        <div className="col-lg-12">
+                            <div className="bs-component">
+                                <fieldset>
+                                    <FormGroup label="Email:  *" htmlFor="exampleInputEmail1">
+                                        <input type="email" value={this.state.email} 
+                                        onChange={e=> this.setState({email: e.target.value})}
+                                        className="form-control" 
+                                        id="exampleInputEmail1" aria-describedby="emailHelp" 
+                                        placeholder="Digite o Email" />
+                                    </FormGroup>
+
+                                    <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
+                                        <input type="password" value={this.state.senha} 
+                                        onChange={e=> this.setState({senha: e.target.value})}
+                                        className="form-control" id="exampleInputPassword1"
+                                        placeholder="Password" />
+                                    </FormGroup>
+
+                                    <button className="btn btn-success" onClick={this.entrar}>Entrar</button>
+                                    <button className="btn btn-danger">Cadastrar</button>
+                                </fieldset>
+                            </div>
+                        </div>
+                    </Card>
+                </div>
+            </div>
+        </div>
+        )
+    }
+}
+
+export default Login;
