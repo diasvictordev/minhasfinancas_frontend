33e7dcac75b35d845b3cdf056cb0e3911956517f
diasvictordev <victor.dias@aluno.ueg.br>
2023-07-14 01:03:05 -0300

    add rotas

--- src/main/App.js
+++ src/main/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import 'bootswatch/dist/flatly/bootstrap.css';
-import Login from './views/login';
-import './custom.css';
-import CadastroUsuario from './views/cadastroUsuario';
+import '../../src/custom.css';
+import Rotas from './rotas';
 
 class App extends React.Component{
   
   render(){
     return(
     <div>
-      <CadastroUsuario/>
+      <Rotas/>
     </div>
     )
   }
